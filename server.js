cf1bef826c2221e1a3915073f2126762c980037c
matthewcv <accounts@matthewcv.com>
2016-03-28 13:33:53 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,20 @@
+/**
+ * Created by matthew on 3/28/2016.
+ */
+
+var express = require('express');
+var morgan = require('morgan');
+
+var app = express();
+
+app.use(morgan("dev"));
+app.use(express.static('node_modules'));
+app.use(function(req,res,next){
+    res.sendFile(__dirname + '/index.html')
+})
+
+app.listen(3001,function(){
+    console.log("app listening on port 3001")
+})
+
+
