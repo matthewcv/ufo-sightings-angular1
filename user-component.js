156adf0d3b710ca3153cbd191f585ff84ad373ae
matthewcv <accounts@matthewcv.com>
2016-04-04 21:00:10 -0500

    Finished implementation of user component

--- user-component.js
+++ user-component.js
@@ -1,16 +1,23 @@
 (function(){
     
-    
+    angular.module('ufo-sightings')                    
+        .component('user', {
+            templateUrl:"user-component.html",
+            controller:UserController
+        });   
+
     function UserController(userService){
         this.userService = userService;
         this.userName = null;
     }
-    
+    UserController.$inject = ['userService']
+
     UserController.prototype.logIn = function(){
         if(this.userName){
             this.userService.logIn(this.userName)
         }
     }
+    
 
     
     
