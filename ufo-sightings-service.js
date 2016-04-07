2e6dc4737a8eafaf74756626b7809d59873e4432
matthewcv <accounts@matthewcv.com>
2016-04-07 18:19:26 -0500

    create ufo-sightings service

---
+++ ufo-sightings-service.js
@@ -0,0 +1,25 @@
+(function(){
+    
+    angular.module("ufo-sightings") 
+        .service("ufoSightingsService", UfoSightingsService)  
+
+    
+    
+    function UfoSightingsService(){
+        
+        
+    }
+    
+    UfoSightingsService.prototype.getUfoSightings = function(page){
+        
+    }
+    
+    UfoSightingsService.prototype.saveUfoSightingReport = function(sighting){
+        
+    }
+    
+    UfoSightingsService.prototype.getUfoSighting = function (id) {
+        
+    }
+    
+})();
