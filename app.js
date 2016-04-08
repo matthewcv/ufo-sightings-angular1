5ff39ccf9ee17e1141b9b56f927f5bf2a3586289
matthewcv <accounts@matthewcv.com>
2016-04-08 12:15:21 -0500

    starting with the component router

--- app.js
+++ app.js
@@ -1,4 +1,9 @@
 (function(){
     angular.module("ufo-sightings", [])  //tells angular that we are creating a module named 'ufo-sightings' with no dependencies.
-        
+    .config(["$locationProvider",function(locationProvider){
+        //use html 5 mode so our urls are nicer
+        locationProvider.html5Mode(true);
+    }])
+    //need to tell the router what the root component is so it has a place to start
+    .value('$routerRootComponent', 'appRoot')    
 })();
