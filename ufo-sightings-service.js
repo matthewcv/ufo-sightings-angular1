64c1fd89e7735e6290da3b9bb7b2e2aed9060b01
matthewcv <accounts@matthewcv.com>
2016-04-07 21:09:38 -0500

    post sighting to server

--- ufo-sightings-service.js
+++ ufo-sightings-service.js
@@ -13,7 +13,15 @@
     UfoSightingsService.$inject = ['$http'];
     
     UfoSightingsService.prototype.getUfoSightings = function(page){
-        return this.http.get("http://localhost:3000/uforeports.sqlite/uforeports?$top=10")
+        
+        var options = { 
+            params : {
+                $top : 10,
+                $skip : (page || 0) * 10,
+                $orderby: "reported desc"
+            }
+        }
+        return this.http.get("http://localhost:3000/uforeports.sqlite/uforeports", options)
             .then(function(response){
                 return response.data
             },function(error){
@@ -23,10 +31,39 @@
     
     UfoSightingsService.prototype.saveUfoSightingReport = function(sighting){
         
+        sighting.reported = new Date();
+        sighting.occurred = dateFormat(sighting.occurred);
+        sighting.reported = dateFormat(sighting.reported);
+        
+        return this.http.post("http://localhost:3000/uforeports.sqlite/uforeports", sighting)
+            .then(function(response){
+                return response.data
+                
+            }, function(error){
+                
+                
+            })
+        
     }
     
     UfoSightingsService.prototype.getUfoSighting = function (id) {
         
     }
     
+    
+    
+    function pad(num){
+        if(num < 10){
+            return "0" + num;
+        }
+        return num;
+    }
+
+    function dateFormat(date){
+        if(isNaN( date.getDate())) {
+            return null;
+        }
+        return date.getFullYear() + "-" + pad(date.getMonth() + 1) + "-" + pad(date.getDate()) + " " + pad(date.getHours()) + ":" + pad(date.getMinutes())
+    }
+
 })();
