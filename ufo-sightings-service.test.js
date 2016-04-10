31a64a55c4e3eb37cfb9cbe46906bbfb240f5d24
matthewcv <accounts@matthewcv.com>
2016-04-10 13:37:50 -0500

    created ufo sightings service test

---
+++ ufo-sightings-service.test.js
@@ -0,0 +1,12 @@
+describe("the ufo sightings service", function(){
+    
+    
+    beforeEach(module('ufo-sightings'));
+
+    it("can get the sightings", inject(function(ufoSightingsService){
+        
+        
+    }));
+    
+    
+})
