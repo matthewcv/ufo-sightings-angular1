c31713145f4ea6f2ca4f9ffe63dce6fd6860338d
matthewcv <accounts@matthewcv.com>
2016-04-10 14:54:26 -0500

    created sighting details component test

---
+++ sighting-details-component.test.js
@@ -0,0 +1,12 @@
+describe("the sighting details component", function(){
+    
+    beforeEach(module('ufo-sightings'));
+    
+    it("should get a ufo sighting when activate by the router", inject(function(){
+        
+        
+        
+    }));
+    
+    
+})
