99dd325dea1bb70dd5d544c2da0b3e6de1c1c002
matthewcv <accounts@matthewcv.com>
2016-04-10 13:34:58 -0500

    filled in states tests

--- states.test.js
+++ states.test.js
@@ -1,10 +1,17 @@
 describe("the states value", function(){
     
-    it("should have 59 items", function(){
-        
-        
-    });
+    beforeEach(module('ufo-sightings'));
     
+    it("should have 59 items", inject(function(states){
+        
+        expect(states.length).toEqual(59);
+        
+    }));
     
+    it("should have Kansas", inject(function(states){
+        
+        expect(states).toContain({"abbreviation":"KS","name":"Kansas"});
+        
+    }))     
     
 })
